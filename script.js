9bc0c2e1470e14e9888fee931142a4ae693b10a3
Pavan Kumar Kundrapu <115059910+Pavankundrapu@users.noreply.github.com>
2024-02-29 22:30:27 +0530

    Update script.js
    changed the relation message

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ friend.addEventListener('click',()=>
   relation.innerHTML = "current relation: friends"
   friend.innerHTML = "added friend"
   remove.innerHTML = "remove"
+  relation.style.color = "green"
 })
 remove.addEventListener('click',()=>
 {
   relation.innerHTML = "current relation: enemies"
   remove.innerHTML = "removed enemy"
   friend.innerHTML = "add friend"
+  relation.style.color = "red"
 })
