e90d6f88a42520f109265f2dd0cb7400e8502049
Pavan Kumar Kundrapu <115059910+Pavankundrapu@users.noreply.github.com>
2024-02-29 22:32:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let relation = document.getElementById('relation')
 
 friend.addEventListener('click',()=>
 {
-  relation.innerHTML = "current relation: friends"
+  relation.innerHTML = "friends"
   friend.innerHTML = "added friend"
   remove.innerHTML = "remove"
   relation.style.color = "green"
 })
 remove.addEventListener('click',()=>
 {
-  relation.innerHTML = "current relation: enemies"
+  relation.innerHTML = "enemies"
   remove.innerHTML = "removed enemy"
   friend.innerHTML = "add friend"
   relation.style.color = "red"
