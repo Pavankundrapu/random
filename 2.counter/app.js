cd8fad283aaf600b7e6ef3b0dd58508042fe454c
Pavan Kumar Kundrapu <115059910+Pavankundrapu@users.noreply.github.com>
2024-03-03 10:36:54 +0530

    Add files via upload

---
+++ 2.counter/app.js
@@ -0,0 +1,35 @@
+let count = 0;
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn)=>
+{//e is event object
+  btn.addEventListener("click",(e)=>{
+      const styles =  e.currentTarget.classList;
+      if(styles.contains("decrease"))
+      {
+        count--;
+      }
+      else if(styles.contains("increase"))
+      {
+        count++;
+      }
+      else
+      {
+        count = 0;
+      }
+      if(count<0)
+      {
+        value.style.color = "red"
+      }
+      else if(count > 0 )
+      {
+        value.style.color = "green"
+      }
+      else if(count === 0){
+        value.style.color = "#222"
+      }
+      value.textContent = count;
+      
+  })
+})
