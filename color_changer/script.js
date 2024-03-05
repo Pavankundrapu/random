5f0848565eaa62187d0ff5d82ad801fc652cd4d9
Pavan Kumar Kundrapu <115059910+Pavankundrapu@users.noreply.github.com>
2024-03-05 22:09:23 +0530

    Add files via upload

---
+++ color_changer/script.js
@@ -0,0 +1,24 @@
+const body = document.body;
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((button)=>{
+  button.addEventListener('click',(e)=>{
+    if(e.target.id === "grey")
+    {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if(e.target.id === "white")
+    {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if(e.target.id === "blue")
+    {
+      body.style.backgroundColor = e.target.id;
+    }
+    else if(e.target.id === "yellow")
+    {
+      body.style.backgroundColor = e.target.id;
+    }
+
+  })
+})
